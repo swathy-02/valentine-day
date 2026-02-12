14280415d3c180941829f557bf1fa289625f0ace
swathy-02 <swathy0201@gmail.com>
2026-02-12 18:17:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,16 @@
-let yesClickedOnce = false;
-
-function yesClicked() {
-  const videoContainer = document.getElementById('videoContainer');
-  const message = document.getElementById('message');
-  const songList = document.getElementById('songList');
-
-  if(!yesClickedOnce){
-    // Play teddy couple kiss video (new YouTube Shorts link)
-    videoContainer.innerHTML = `
-      <iframe width="400" height="300" src="https://www.youtube.com/embed/rX4PYXRpIRY?autoplay=1&mute=1"
-      title="Teddy Couple Kiss" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-    `;
-    message.innerText = "💖 Look at our cute teddy couple! 💖";
-    yesClickedOnce = true;
-  } else {
-    message.innerText = "Ok 💖 You are the best! 😘";
-    songList.style.display = 'block';
-  }
+function goYes() {
+  window.location.href = "yes.html";
 }
 
-function noClicked() {
-  const videoContainer = document.getElementById('videoContainer');
-  const message = document.getElementById('message');
-  const songList = document.getElementById('songList');
+const noBtn = document.getElementById("noBtn");
 
-  // Play angry teddy video (previous link)
-  videoContainer.innerHTML = `
-    <iframe width="400" height="300" src="https://www.youtube.com/embed/-vmR9Pieii4?autoplay=1&mute=1"
-    title="Angry Teddy" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-  `;
-  message.innerText = "😠 Oh no! Don't be angry, pick a song to feel better!";
-  songList.style.display = 'block';
-}
+noBtn.addEventListener("mouseover", () => {
+  const maxX = window.innerWidth - noBtn.offsetWidth;
+  const maxY = window.innerHeight - noBtn.offsetHeight;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
